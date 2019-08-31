f41e2c094d148b57557a0250236348a0dc7fcafa
Lou Marco <loumarco@hotmail.com>
2019-08-31 15:25:18 -0500

    Pass to github so github can alert Jenkins for trigger

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('<h1>Hello From Node!!</h1>\n<h2>For feature Branch</h2>First push from github to Jenkins'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`listening on port ${port}!`))
