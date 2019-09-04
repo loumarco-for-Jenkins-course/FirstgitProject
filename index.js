97961408275c95deb04867f79a0f13b04908934c
Lou Marco <loumarco@hotmail.com>
2019-09-04 10:39:13 -0500

    Test #2 with Jenkins AND project email configs

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('<h1>Hello From Node!!</h1>\n<h2>For email notification</h2>\nPushing this change for Jenkins auto build'))
+app.get('/', (req, res) => res.send('<h1>Hello From Node!!</h1>\n<h2>Email notification</h2>\nPushing this change for Jenkins auto build'))
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
