91adf6a243649cb3f13f46e7970a0f19780e7f4e
Lou Marco <loumarco@hotmail.com>
2019-09-12 12:06:40 -0500

    another shot no slash in URL

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('<h1>Hello From Node!!</h1>\n<h2>Email notification</h2>\nPushing this change for Jenkins auto build'))
 
-app.listen(port, () => console.log(`A Change for lab chapter 3 ${port}!`))
+app.listen(port, () => console.log(`An Change for lab chapter 3 ${port}!`))
